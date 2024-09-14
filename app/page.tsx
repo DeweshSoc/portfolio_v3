11994b05cc6f99632b1ab15820724124c9f445e4
DeweshSoc <deweshjha4@gmail.com>
2024-09-14 16:10:36 +0530

    complete projects and sentry

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import Grid from "@/components/Grid";
 // import Clients from "@/components/Clients";
 // import Approach from "@/components/Approach";
 // import Experience from "@/components/Experience";
-// import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import RecentProjects from "@/components/RecentProjects";
 
 const Home = () => {
     return (
@@ -18,7 +18,7 @@ const Home = () => {
                 <FloatingNav navItems={navItems} />
                 <Hero />
                 <Grid />
-
+                <RecentProjects/>
             </div>
         </main>
     );
---
+++ sentry.client.config.ts
@@ -0,0 +1,21 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+    dsn: "https://774a6789c146f8295797cf98cf8f72a5@o4507950597668864.ingest.de.sentry.io/4507950602649680",
+
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+    integrations: [
+        Sentry.feedbackIntegration({
+            // Additional SDK configuration goes in here, for example:
+            colorScheme: "dark",
+        }),
+    ],
+});
